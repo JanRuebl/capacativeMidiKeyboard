262e1a5f05a6a625b090970557eaa3b516d98a3f
Jan Rübl <janruebl@yahoo.com>
2020-02-08 22:12:35 +0100

    first commit

---
+++ midi_keyboard_mega.ino
@@ -0,0 +1,47 @@
+#include <CapacitiveSensor.h>
+
+
+// hier werden die korken auf digitalPins gelegt. pin 3 ist messpin. 
+
+CapacitiveSensor korken_22 = CapacitiveSensor(3,22); // erst messpin (3), dann digitalpin 22
+
+bool korken_22_isInUse = false;
+
+
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  long korken_22_val = korken_22.capacitiveSensor(30);
+
+
+
+if( korken_22_val > 0){
+     Serial.print( korken_22_val);  
+  }
+
+//  if(korken_22_val > 0 && !korken_22_isInUse){
+//          korken_22_isInUse = true;
+//          Serial.print("22 korken geht");
+//    }
+//  if(korken_22_val == 0 && korken_22_isInUse){
+//          korken_22_isInUse = false;
+//          Serial.print("22 korken aus");
+//    }
+
+
+
+
+    
+
+}
+
+
+void NoteOn(int channel, int note, int velocity) {
+  
+  }
